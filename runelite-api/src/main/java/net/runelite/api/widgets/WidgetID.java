fc6a65407f8b538c75df22aab4cd04c1e918e10d
RuneLite Cache-Code Autoupdater <mii7303+rlccau@gmail.com>
2021-12-09 11:34:17 +0000

    Update Widget IDs to 2021-12-09-rev202
    Lost widget 609.2

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -300,9 +300,9 @@ public final class WidgetID
 		static final int WINDOW_BORDERS = 2;
 		static final int HISTORY_BUTTON = 3;
 		static final int BACK_BUTTON = 4;
-		static final int OFFER_CONTAINER = 24;
-		static final int OFFER_DESCRIPTION = 25;
-		static final int OFFER_PRICE = 26;
+		static final int OFFER_CONTAINER = 25;
+		static final int OFFER_DESCRIPTION = 26;
+		static final int OFFER_PRICE = 27;
 		static final int OFFER_CONFIRM_BUTTON = 27;
 	}
 
@@ -849,10 +849,10 @@ public final class WidgetID
 
 	static class SettingsSide
 	{
-		static final int CAMERA_ZOOM_SLIDER_TRACK = 98;
-		static final int MUSIC_SLIDER = 27;
-		static final int SOUND_EFFECT_SLIDER = 41;
-		static final int AREA_SOUND_SLIDER = 55;
+		static final int CAMERA_ZOOM_SLIDER_TRACK = 100;
+		static final int MUSIC_SLIDER = 29;
+		static final int SOUND_EFFECT_SLIDER = 43;
+		static final int AREA_SOUND_SLIDER = 57;
 	}
 
 	static class Settings
